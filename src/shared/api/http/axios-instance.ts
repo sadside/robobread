49b5022b4e7b752397eda673ce21d9e563ad19d1
FofuHupopo <fofuhupopo@gmail.com>
2024-06-26 20:46:21 +0500

    change urls

--- src/shared/api/http/axios-instance.ts
+++ src/shared/api/http/axios-instance.ts
@@ -1,13 +1,10 @@
 import axios, {AxiosError, CreateAxiosDefaults} from "axios";
 
-// export const API_URL_CLIENT = 'https://thebaza.ru/api/';
-// export const API_URL = 'http://127.0.0.1:8000/api/';
-
+// export const API_URL = "http://127.0.0.1:8000/api/";
 // export const API_URL_CLIENT = "http://127.0.0.1:8000/api/";
-export const API_URL = "http://127.0.0.1:8000/api/";
 
-export const API_URL_CLIENT = "http://127.0.0.1:8000/api/";
-// export const API_URL = "http://server:8000/api/";
+export const API_URL = "http://192.168.114.11/api/";
+export const API_URL_CLIENT = "http://192.168.114.11/api/";
 
 const getContentType = () => ({
     "Content-Type": "application/json"
